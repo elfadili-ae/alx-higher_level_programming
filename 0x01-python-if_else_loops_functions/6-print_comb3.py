90a604234341233dd290e994b1fe6d9fd69681e6
elfadili-ae <elfadili.ae@gmail.com>
2023-06-06 11:45:58 +0100

    Script: added 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range (i, 10):
+        if i == 8 and j == 9:
+            break;
+        if i != j:
+            print("{}{}".format(i, j), end=", ")
+print("89")

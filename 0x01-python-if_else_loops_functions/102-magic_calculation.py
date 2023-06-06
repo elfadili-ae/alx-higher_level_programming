56486bd86cbebfb5f7f672cfb76f875bdd18ec57
elfadili-ae <elfadili.ae@gmail.com>
2023-06-06 14:17:01 +0100

    Fix: style

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-import dis
 def magic_calculation(a, b, c):
     if a < b:
         return c
-    if c > b:
+    elif c > b:
         return a + b
     else:
         return a + b + c
-
-#dis.dis(magic_calculation)

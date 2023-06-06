626d5cf301e9fb03fe0e42b83fe72fe77acb3f57
elfadili-ae <elfadili.ae@gmail.com>
2023-06-06 11:49:07 +0100

    Style: py style fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range (i, 10):
+    for j in range(i, 10):
         if i == 8 and j == 9:
-            break;
+            break
         if i != j:
             print("{}{}".format(i, j), end=", ")
 print("89")

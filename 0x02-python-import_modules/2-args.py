ffbb44d686f2b2bd1be29aacb5db1bec3b82cb8b
elfadili-ae <elfadili.ae@gmail.com>
2023-06-08 11:22:43 +0100

    Fix: plural args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 if __name__ == "__main__":
     import sys
 
-    if (len(sys.argv) - 1) == 0:
+    argc = len(sys.argv) - 1
+    if argc == 0:
         print("0 arguments.")
         sys.exit(0)
 
-    print("{} arguments:".format(len(sys.argv) - 1))
+    ps = "s" if argc > 1 else ""
+    print("{} argument{}:".format(argc, ps))
     i = 1
     for arg in sys.argv[1:]:
         print("{}: {}".format(i, arg))

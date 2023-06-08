d2392cd8a219427bdd5e4917ba609240149e0dd9
elfadili-ae <elfadili.ae@gmail.com>
2023-06-08 11:18:37 +0100

    Fix: no arguments case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,14 @@
 if __name__ == "__main__":
     import sys
 
+    if (len(sys.argv) - 1) == 0:
+        print("0 arguments.")
+        sys.exit(0)
+
     print("{} arguments:".format(len(sys.argv) - 1))
     i = 1
     for arg in sys.argv[1:]:
         print("{}: {}".format(i, arg))
         i += 1
+else:
+    pass

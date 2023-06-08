485ee53bdc9c5b93bea10a5063aba46b32654a5f
elfadili-ae <elfadili.ae@gmail.com>
2023-06-08 13:46:51 +0100

    Fix: typo

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     from calculator_1 import add, sub, mul, div
 
-    argc = len(sys.argv) - 1
+    argc = len(argv) - 1
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[3])
-    op = sys.argv[2]
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    op = argv[2]
 
     if op == "+":
         print("{} + {} = {}".format(num1, num2, add(num1, num2)))
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     elif op == "/":
         print("{} / {} = {}".format(num1, num2, div(num1, num2)))
     else:
-        print("Unknown opator. Available opators: +, -, * and /")
-        sys.exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
 else:
     pass

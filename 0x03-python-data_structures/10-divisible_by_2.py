7e2a3667634cfa447af36a8748e8d288357ff960
elfadili-ae <elfadili.ae@gmail.com>
2023-06-10 11:38:20 +0100

    Script: added 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    res = []
+    for num in my_list:
+        if num % 2 == 0:
+            res.append(True)
+        else:
+            res.append(False)
+    return res

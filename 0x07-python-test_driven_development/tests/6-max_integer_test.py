28d08a517e734ceaa9c5d9d694ce2a504d942874
elfadili-ae <elfadili.ae@gmail.com>
2023-07-03 01:15:36 +0100

    Fix: code style

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
 import unittest
+
 max_integer = __import__("6-max_integer").max_integer
 
 """unittest for max_integer function
@@ -7,42 +8,36 @@ max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """test cases for max_integer
-    """
+    """test cases for max_integer"""
+
     def test_ModuleDoc(self):
-        """check the module's doc style
-        """
+        """check the module's doc style"""
         self.assertTrue(__import__("6-max_integer").__doc__)
 
     def test_FunctionDoc(self):
-        """check the function's doc style
-        """
+        """check the function's doc style"""
         self.assertTrue(__import__("6-max_integer").max_integer.__doc__)
 
     def test_emptyList(self):
-        """empty list case
-        """
+        """empty list case"""
         self.assertEqual(None, max_integer([]))
 
     def test_oneElementInList(self):
-        """one element in the list case
-        """
+        """one element in the list case"""
         self.assertEqual(13, max_integer([13]))
 
     def test_argNotList(self):
-        """arg not a list
-        """
+        """arg not a list"""
         self.assertRaises(TypeError, max_integer("not a list"))
 
     def test_NoArg(self):
-        """no argument
-        """
+        """no argument"""
         self.assertRaises(TypeError, max_integer())
 
     def test_listMixedNoInt(self):
-        """list elements not all int
-        """
-        self.assertRaises(TypeError, max_integer([13, "h", 44, 'C']))
+        """list elements not all int"""
+        self.assertRaises(TypeError, max_integer([13, "h", 44, "C"]))
+
 
 if __name__ == "__main__":
     unittest.main()

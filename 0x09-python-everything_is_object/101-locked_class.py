4595b0ba71f50829a2338e8321d734be7a923cb7
elfadili-ae <elfadili.ae@gmail.com>
2023-07-04 14:00:18 +0100

    Fix: used the __slots__ attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,12 +5,4 @@
 
 class LockedClass:
     """prevent user from creating new instance attributes"""
-
-    def __setattr__(self, attr, val):
-        """set attribute"""
-        if attr != "first_name":
-            raise AttributeError(
-                "'LockedClass' object has no attribute {}".format(attr)
-            )
-        else:
-            self.__dict__[attr] = val
+    __slots__ = ['first_name']

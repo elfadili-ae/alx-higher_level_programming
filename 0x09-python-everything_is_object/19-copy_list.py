3e46a6204a04a44b0f7420472186c28eb102fc06
elfadili-ae <elfadili.ae@gmail.com>
2023-07-04 12:07:01 +0100

    Fix: pycodestyle

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def copy_list(l):
-    return l.copy()
+def copy_list(ridiculouslyLongVariableNameThatMakesYourFingersCramp):
+    return ridiculouslyLongVariableNameThatMakesYourFingersCramp.copy()

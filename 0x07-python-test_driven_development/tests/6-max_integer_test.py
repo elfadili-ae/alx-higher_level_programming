a2753334d2cd64a213e7b01259b25c6a5a13a21b
elfadili-ae <elfadili.ae@gmail.com>
2023-07-05 11:29:12 +0100

    Fix: added more cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,7 +3,9 @@
 """
 
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+
+max_integer = __import__("6-max_integer").max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     """test cases for max_integer"""
@@ -17,6 +19,18 @@ class TestMaxInteger(unittest.TestCase):
         """check the function's doc style"""
         doc = max_integer.__doc__
         self.assertTrue(len(doc) > 1)
+
+    def test_MaxAtBeginning(self):
+        """max at the beginning of the list case"""
+        self.assertEqual(100, max_integer([100, 5, 4]))
+
+    def test_MaxAtMid(self):
+        """max at the middle of the list case"""
+        self.assertEqual(45, max_integer([10, 5, 45, 44, 4]))
+
+    def test_MaxAtEnd(self):
+        """max at the end of the list case"""
+        self.assertEqual(544, max_integer([10, 5, 45, 44, 4, 544]))
 
     def test_emptyList(self):
         """empty list case"""
@@ -38,6 +52,7 @@ class TestMaxInteger(unittest.TestCase):
         """list elements not all int"""
         with self.assertRaises(TypeError):
             max_integer([13, "h", 44, "C"])
+
 
 if __name__ == "__main__":
     unittest.main()

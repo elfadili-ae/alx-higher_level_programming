ee00afdb58ecb2471439b76ab7dc492b4fef8b2c
elfadili-ae <elfadili.ae@gmail.com>
2023-07-10 22:19:49 +0100

    Fix: validate widith also

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -14,6 +14,7 @@ class Rectangle(BaseGeometry):
             width: width of the rectangle
             height: height of the rectangle
         """
+        self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height

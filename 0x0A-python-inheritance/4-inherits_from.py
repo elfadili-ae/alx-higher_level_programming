4f4354571c2e9fafbc2e2b107b4aa20f7b515c4f
elfadili-ae <elfadili.ae@gmail.com>
2023-07-10 13:23:21 +0100

    Fix: check directly and inderectly inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
     Returns:
         True (inherits) False (otherwise)
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class

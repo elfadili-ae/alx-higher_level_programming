dc8aedd4275123ae28344865a8ac3a5313d16681
elfadili-ae <elfadili.ae@gmail.com>
2023-07-16 11:42:35 +0100

    Script: added create, load from file to base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,33 @@ class Base:
             else:
                 list_dictionaries = [ele.to_dictionary() for ele in list_objs]
                 f.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return the list of JSON string representation"""
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """return an instance with all attributes"""
+        if dictionary is not None:
+            if cls.__name__ == "Rectangle":
+                insta = cls(1, 1)
+            elif cls.__name__ == "Square":
+                insta = cls(1)
+            val =insta.update(**dictionary)
+            return insta
+
+    @classmethod
+    def load_from_file(cls):
+        """return list of instances"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r', encoding="utf-8") as f:
+                li = Base.from_json_string(f.read())
+                return [cls.create(**ele) for ele in li]
+        except IOError:
+            return []

200a1fb3965f5918fa8b6b96bc73b4c81c791c1f
elfadili-ae <elfadili.ae@gmail.com>
2023-07-17 18:46:17 +0100

    Base: to_json_string static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """JSON representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:

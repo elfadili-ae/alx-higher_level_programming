254addc5692e596a475b8b096ac697a8f1fd1e7d
elfadili-ae <elfadili.ae@gmail.com>
2023-07-17 18:39:31 +0100

    Base: fixed list_objs case

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """JSON representation of list_dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
@@ -31,7 +31,7 @@ class Base:
         """save the obj representation to JSON file"""
         filename = cls.__name__ + ".json"
         with open(filename, "w", encoding="utf-8") as f:
-            if list_objs is None:
+            if list_objs is None or list_objs == []:
                 f.write("[]")
             else:
                 list_dictionaries = [ele.to_dictionary() for ele in list_objs]

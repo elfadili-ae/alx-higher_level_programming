78de625c7822df7904f08e34344d6e59b8aa03ca
elfadili-ae <elfadili.ae@gmail.com>
2023-08-15 16:30:25 +0100

    Fix: convert db

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,8 @@
 -- convert table to utf8
-USE hbtn_0c_0
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+
 ALTER TABLE second_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

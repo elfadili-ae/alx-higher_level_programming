8ec955f6e28fc87f4e34601e0dbbc9bcb9725c4b
elfadili-ae <elfadili.ae@gmail.com>
2023-08-16 10:59:51 +0100

    Fix: quote

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- show granted permissions for user_0d_1
--- show granted permissions for user_0d_2
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- show granted permissions for user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

5ab5aa29ce12f8a309f4580d5602a444b2936ef4
elfadili-ae <elfadili.ae@gmail.com>
2023-09-11 13:21:57 +0100

    Fix: format

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const argv = process.argv;
 
 if (argv[2] && Number(argv[2])) {
-  console.log(parseInt(argv[2]));
+  console.log(`My number: ${parseInt(argv[2])}`);
 } else {
   console.log('Not a number');
 }

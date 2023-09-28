01bb91d6a8c2d156f63ab1b89219f5469a6aecf3
elfadili-ae <elfadili.ae@gmail.com>
2023-09-28 14:18:02 +0100

    Update: changed to /bin/bash

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
-# send a request to the URL > usage: ./0-body_size.sh <url>
-
-curl -sI $1 | grep "Content-Length" | cut -d" " -f 2
+#!/bin/bash
+# send a request to the URL
+curl -s "$1" | wc -c

ad940e8828a852c97f7cacaf9221dc6a83961e2f
elfadili-ae <elfadili.ae@gmail.com>
2023-09-28 20:36:40 +0100

    Fix: efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,29 +2,32 @@
 """Find the peak in a list."""
 
 
-def find_peak(list_of_integers):
+def find_peak(nums):
     """Peak finder."""
-    n = len(list_of_integers)
+    n = len(nums)
     if n == 0:
         return None
 
     mid = int(n / 2)
     if mid - 1 < 0 and mid + 1 >= n:
-        return list_of_integers[mid]
+        return nums[mid]
 
     if mid - 1 < 0:
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            return list_of_integers[mid + 1]
+        if nums[mid] < nums[mid + 1]:
+            return nums[mid + 1]
         else:
-            return list_of_integers[mid]
+            return nums[mid]
 
     if mid + 1 >= n:
-        if list_of_integers[mid] > list_of_integers[mid - 1]:
-            return list_of_integers[mid]
+        if nums[mid] > nums[mid - 1]:
+            return nums[mid]
         else:
-            return list_of_integers[mid - 1]
+            return nums[mid - 1]
 
-    if list_of_integers[mid] > list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[mid:])
+    if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
+        return nums[mid]
+
+    if nums[mid + 1] < nums[mid - 1]:
+        return find_peak(nums[:mid])
     else:
-        return find_peak(list_of_integers[:mid])
+        return find_peak(nums[mid:])

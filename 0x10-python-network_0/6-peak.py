dba4e24198f5d21a1a791a0c0b9d565ac5704a43
elfadili-ae <elfadili.ae@gmail.com>
2023-09-28 20:04:02 +0100

    divide and conquer

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 """Find the peak in a list."""
 
+
 def find_peak(list_of_integers):
     """Peak finder."""
-
-    if len(list_of_integers) == 0:
+    n = len(list_of_integers)
+    if n == 0:
         return None
-    return (max(list_of_integers))
+
+    mid = int(n / 2)
+    if mid - 1 < 0 and mid + 1 >= n:
+        return list_of_integers[mid]
+
+    if mid - 1 < 0:
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            return list_of_integers[mid + 1]
+        else:
+            return list_of_integers[mid]
+
+    if mid + 1 >= n:
+        if list_of_integers[mid] > list_of_integers[mid - 1]:
+            return list_of_integers[mid]
+        else:
+            return list_of_integers[mid - 1]
+
+    if list_of_integers[mid] > list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[mid:])
+    else:
+        return find_peak(list_of_integers[:mid])

585f8249c38b5204bbd172ff81b6acacb058d0ba
elfadili-ae <elfadili.ae@gmail.com>
2023-09-29 11:50:00 +0100

    Fix: used get to access dict

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,8 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    print(dict(response.headers)["X-Request-Id"])
+    with urllib.request.urlopen(url) as response:
+        print(dict(response.headers).get("X-Request-Id"))

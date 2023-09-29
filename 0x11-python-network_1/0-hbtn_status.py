937d2c70d3f88ad6d6e0f4e09381004212875f67
elfadili-ae <elfadili.ae@gmail.com>
2023-09-29 11:34:25 +0100

    Fix: format style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,6 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("    - type: ", end="")
-    print(type(html))
-    print("    - content: ", end="")
-    print(html)
-    print("    - utf8 content: ", end="")
-    print(html.decode('utf8'))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
